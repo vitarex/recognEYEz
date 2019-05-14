3c02793e4cc070668e8f965c03f20a9a338a92f5
Gabor Gulyas <gabor@gulyas.info>
2019-05-14 21:07:38 +0200

    still debugging

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from webapp import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    print("Server starting...")
+    print("Server starting.")
     # app.run(debug=False)
     app.run(host="152.66.170.159", debug=True)
-    print("Server started")
+    print("Server stopped.")
