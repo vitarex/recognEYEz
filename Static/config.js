4e446e3c54b63da438cf98a921ac3d494c25eee8
Villányi Bálint <vilanyib@gmail.com>
2019-07-29 10:30:56 +0200

    Delete setting button

--- Static/config.js
+++ Static/config.js
@@ -16,4 +16,27 @@ $(document).ready(function() {
     if (e.target.value == -1) $("#url-" + e.target.options[e.target.options.length-1].id).prop("disabled", false);
     else $("#url-" + e.target.options[e.target.options.length-1].id).prop("disabled", true);
   });
+
+  $(".delete-setting").click(function(e) {
+    delete_setting(
+      e.target.id.replace("delete-setting-", ""),
+      function() {
+        var fieldset = $(this).parent()
+        var radio_input = $(fieldset).siblings("#" + fieldset.id.replace("-fieldset", ""))
+        fieldset.remove()
+        radio_input.remove()
+      }.bind(e.target));
+  });
 });
+
+function delete_setting(setting_name, success_func) {
+  $.ajax({
+    url: $SCRIPT_ROOT + "/delete_camera_config",
+    data: { setting_name: setting_name},
+    type: "POST",
+    success: success_func,
+    error: function(error) {
+      alert(JSON.parse(error).message);
+    }
+  });
+}
